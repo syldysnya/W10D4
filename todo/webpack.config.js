e6729f10d87df74b2a33ac970ebd88d559a9dc84
tahj <tahjharris00@gmail.com>
2021-04-29 13:13:32 -0500

    fix git push

---
+++ todo/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: './todo_redux.jsx',
+    output: {
+        filename: './bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['.js', '.jsx', '*']
+    }
+};
